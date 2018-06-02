20c6bd0445ec40bd361ab1aab3d33c2afb141656
Charles Yach <CharlesY712@gmail.com>
2018-06-02 13:24:56 -0600

    only commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', function(socket) {
+  io.emit('chat message', 'a user connected');
+  socket.on('disconnect', function() {
+    io.emit('chat message', 'user disconnected');
+  })
+})
+
+io.on('connection', function(socket) {
+
+
+  socket.on('chat message', function(msg) {
+    socket.broadcast.emit('chat message', ` ${msg.user}: ${msg.value}`);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+})
